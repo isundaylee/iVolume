ea3fd8b9eb6fafa9c293e3bcabec07c19a7332d6
Jiahao Li <isundaylee.reg@gmail.com>
2017-10-13 04:31:11 -0400

    Initial Commit

---
+++ iVolume/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  iVolume
+//
+//  Created by Jiahao Li on 10/13/17.
+//  Copyright © 2017 Jiahao Li. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ iVolume/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iVolume
+//
+//  Created by Jiahao Li on 10/13/17.
+//  Copyright © 2017 Jiahao Li. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
